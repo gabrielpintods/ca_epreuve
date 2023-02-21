16d98eb35a99994206840887ade848939c320c89
gabrielpintods <gabrielpintodasilvapro@gmail.com>
2023-02-21 12:09:04 +0100

    feu04: done

--- feu/feu04.js
+++ feu/feu04.js
@@ -14,7 +14,6 @@ function readFile(fileName) {
     return data;      
 }
 
-
 // return 2d array of the str, each ligne is a new array of the second dimension
 function strToArr (str) {
     let arr = [];
@@ -29,7 +28,7 @@ function strToArr (str) {
         } else {
             arr[i][k] = parseInt(str[j]);
             if(isNaN(arr[i][k])) {
-                arr[i][k] = '.';
+                arr[i][k] = 0;
             }
             k++;
         }
@@ -37,58 +36,53 @@ function strToArr (str) {
     return arr;
 }
 
-// return a 2d array corresponding of sudoku pattern 
-function arrToSdk(arr) {
-    let sdk = [];
-    let l = 0;
-    let i = 0; 
-    let j = 0;
-    let saveI = i;
-    let saveJ = j;
-    let sqrtLength = Math.sqrt(arr.length);
-    let sqrtLengthI = Math.sqrt(arr.length)
-    for(let k = 0; k < arr.length; k++) {
-        sdk[k] = [];
-        while(i < sqrtLengthI) {
-            while(j < sqrtLength) {
-                sdk[k][l] = arr[i][j];
-                l++;
-                j++;
+// display function
+function displaySdk(sdk) {
+    let length = Math.sqrt(sdk.length)
+    let heightSquare = length;
+    let widthSquare = length;
+    for(let i = 0; i < sdk.length; i++) {
+        if(i === heightSquare) {
+            heightSquare += length;
+            console.log("-----------");
+        }
+        for(let j = 0; j < sdk[i].length; j++) {
+            if(j === widthSquare) {
+                process.stdout.write('|' + sdk[i][j].toString());
+                widthSquare += length;
+            } else {
+                process.stdout.write(sdk[i][j].toString());
             }
-            j = saveJ;
-            i++;
         }
-        j = sqrtLength;
-        sqrtLength += Math.sqrt(arr.length);
-        if(sqrtLength > arr.length) { 
-            sqrtLengthI += Math.sqrt(arr.length);
-            saveI = i;
-            sqrtLength = Math.sqrt(arr.length);
-            j = 0;
-        } else {
-            i = saveI;
-        }
-        l = 0;
-        saveJ = j;
+        widthSquare = length;
+        console.log();
     }
-    return sdk;
 }
 
-// return true if nbr is already in the square
-function inSquare(nbr, square) {
-    let isIn = false;
-    for(let i = 0; i < square.length && !isIn; i++) {
-        if(square[i] === nbr) {
-            isIn = true;
+// check if i is not in the i line
+function inLine (k, sdk, i) {
+    for (let j = 0; j < sdk.length; j++) {
+        if (sdk[i][j] === k) {
+            return true
         }
     }
-    return isIn;
+    return false;
 }
 
-// return the range of index square next to (in line) the index 
-function findIndexLine(sdk, index) {
-    let indexBtwn = [0, Math.sqrt(sdk.length)];
+// check if k is not in the j column 
+function inColumn (k, sdk, j) {
+    for (let i = 0; i < sdk.length; i++) {
+        if (sdk[i][j] == k) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// return the range of index square next to the index 
+function findIndex(sdk, index) {
     let sqrt = Math.sqrt(sdk.length);
+    let indexBtwn = [0, sqrt];
     for(let i = 0; i <= sdk.length && indexBtwn[1] < sdk.length; i += sqrt) {
         if(index >= indexBtwn[1]) {
             indexBtwn.splice(0, 2, indexBtwn[1], indexBtwn[1] + sqrt);
@@ -97,167 +91,65 @@ function findIndexLine(sdk, index) {
     return indexBtwn;
 }
 
-// return the range of index square next to (in column) the index
-function findIndexColumn(sdk, index) {
-    let indexBtwn = [index, index];
-    let sqrt = Math.sqrt(sdk.length);
-    let find1 = false;
-    let find2 = false;
-    while(!find1 || !find2) {
-        if(indexBtwn[0] - sqrt >= 0) {
-            indexBtwn[0] -= sqrt;
-        } else {
-            find1 = true;
-        }
-        if(indexBtwn[1] + sqrt < sdk.length) {
-            indexBtwn[1] += sqrt;
-        } else {
-            find2 = true;
-        }
-    }   
-    return indexBtwn;
-}
-
-function inLine(nbr, sdk, i, j) {
-    let isIn = false;
-    let indexBtwnI = findIndexLine(sdk, i);
-    let indexBtwnJ = findIndexLine(sdk, j);
-    for(let k = indexBtwnI[0]; k < indexBtwnI[1] && !isIn; k++) {
-        for(let l = indexBtwnJ[0]; l < indexBtwnJ[1] && !isIn; l++) {
-            if(nbr === sdk[k][l]) {
-                isIn = true;
+// check if the element at i and j index is not in the bloc square 
+function inSquare(k, sdk, i, j) {
+    let line = findIndex(sdk, i);
+    let column = findIndex(sdk, j);
+    for(let m = line[0]; m < line[1]; m++) {
+        for(let l = column[0]; l < column[1]; l++) {
+            if(k === sdk[m][l]) {
+                return true;
             }
         }
     }
-    return isIn;
+    return false;
 }
 
-function inColumn(nbr, sdk, i, j) {
-    let isIn = false; 
-    let indexBtwnI = findIndexColumn(sdk, i);
-    let indexBtwnJ = findIndexColumn(sdk, j);
-    let sqrt = Math.sqrt(sdk.length);
-    for(let k = indexBtwnI[0]; k <= indexBtwnI[1] && !isIn; k += sqrt) {
-        for(let l = indexBtwnJ[0]; l <= indexBtwnJ[1] && !isIn; l += sqrt) {
-            if(nbr === sdk[k][l]) {
-                isIn = true;
+// find the index of pos 
+function findIndexPos (pos, sdk) {
+    let cpt = 0;
+    let coord = [];
+    for(let i = 0; i < sdk.length; i++) {
+        for(let j = 0; j < sdk[i].length; j++) {
+            if(cpt === pos) {
+                coord = [i, j];
+                return coord;
             }
-        }  
-    }
-    return isIn;
-}
-
-function find(sdk, i, j, nbr) {
-    let ret = '.';
-    if(nbr === -1) {
-        nbr = 1;
-    } else {
-        nbr++;
-    }
-    for(let k = 1; k < 10; k++) {
-        if(!inSquare(nbr, sdk[i]) && !inColumn(nbr, sdk, i, j) && !inLine(nbr, sdk, i, j)) {
-            ret = nbr;
-            break;
-        } else {
-            nbr++;
-        }
-        if(nbr >= 10) {
-            nbr = 1;
-        } 
-    }
-    return ret;
-}
-
-// cross the sdk line by line and when the element is '.', try to find the number.
-function resolve(sdk) {
-    let i = 0;
-    let j = 0;
-    let lap = 0;
-    let lineSq = 0;
-    let saveIJ = [-1,-1];
-    while(i < sdk.length) {
-        let line = findIndexLine(sdk, i);
-        i = line[0];
-        while(i < line[1] && lineSq < Math.sqrt(sdk.length)) {
-            let column = findIndexLine(sdk, j);
-            j = column[0];
-            while(j < column[1]) {
-                if(sdk[i][j] === '.') {
-                    sdk[i][j] = find(sdk, i, j, -1);
-                    if(sdk[i][j] === '.') {
-                        sdk[saveIJ[0]][saveIJ[1]] = find(sdk, saveIJ[i], save[j]);
-                    }
-                    saveIJ = [i,j];
-                }
-                j++;
-                lap++;
-            }
-            i++;
-            if(lap >= sdk.length) {
-                j = column[0];
-                j += Math.sqrt(sdk.length);
-                i = line[0];
-                lineSq++;
-                lap = 0;
-            } else {
-                j = column[0];
-            }
-        }
-        i = line[0];
-        i += Math.sqrt(sdk.length);
-        j = 0;
-        lineSq = 0;
-    }
-}
-
-function displaySdk(sdk) {
-    let i = 0;
-    let j = 0;
-    let lap = 0;
-    let lineSq = 0;
-    while(i < sdk.length) {
-        let line = findIndexLine(sdk, i);
-        i = line[0];
-        while(i < line[1] && lineSq < Math.sqrt(sdk.length)) {
-            let column = findIndexLine(sdk, j);
-            j = column[0];
-            while(j < column[1]) {
-                if(j === column[1] - 1) {
-                    process.stdout.write(sdk[i][j].toString());
-                } else {
-                    process.stdout.write(sdk[i][j].toString() + '|');
-                }
-                j++;
-                lap++;
-            }
-            i++;
-            if(lap >= sdk.length) {
-                j = column[0];
-                j += Math.sqrt(sdk.length);
-                i = line[0];
-                lineSq++;
-                lap = 0;
-                console.log();
-            } else {
-                j = column[0];
-                process.stdout.write('  ');
-            }
-        }
-        i = line[0];
-        i += Math.sqrt(sdk.length);
-        j = 0;
-        lineSq = 0;
-        if(i < sdk.length - 1) {
-            console.log("-------------------");
+            cpt++;
         }
     }
+    return coord;
 }
 
+// main and recursive function that search element by element the right combination and use the backtracking algorithm
+function resolve (sdk, pos) {
+    if (pos === sdk.length * sdk.length) {
+        return true;
+    }
+    // find the right index of the position
+    let coordIJ = findIndexPos(pos, sdk);
+    let i = coordIJ[0];
+    let j = coordIJ[1];
+    if (sdk[i][j] !== 0) {
+        return resolve(sdk, pos+1);
+    }
+    for (let k = 1; k < 10; k++) {
+        if (!inLine(k,sdk,i) && !inColumn(k,sdk,j) && !inSquare(k,sdk,i,j)) {
+            sdk[i][j] = k;
+            if ( resolve (sdk, pos+1) ) {
+                return true;
+            }
+        }
+    }
+    sdk[i][j] = 0;
+    return false;
+}
+
+// normalize the sdk into a 2d array and call resolve
 function exec(arg) {
     let str = readFile(arg[0]);
     let sdk = strToArr(str);
-    sdk = arrToSdk(sdk);
-    resolve(sdk);
+    resolve(sdk, 0);
     displaySdk(sdk);
 }
 
